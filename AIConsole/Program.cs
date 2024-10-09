ebe9c371eedabe0334f587d9eda91feeef7776dc
Mike Hjort Dalgas <MOG@kmd.dk>
2024-10-09 13:54:28 +0200

    Nu med Studica

--- AIConsole/Program.cs
+++ AIConsole/Program.cs
@@ -7,7 +7,12 @@ using Microsoft.SemanticKernel.ChatCompletion;
 using Microsoft.SemanticKernel.Connectors.OpenAI;
 using Microsoft.SemanticKernel.Plugins.OpenApi;
 using SemanticKernelLibrary;
+using System.Net.Http.Headers;
 
+
+#region longToken
+var token = "eyJhbGciOiJSUzI1NiIsImtpZCI6InpGTDB5ZGF2NDgxSHJDT2M0bk9MdTR2cDB2SDZxeTBvUS1SdlBzQW1vNlkiLCJ0eXAiOiJKV1QifQ.eyJhdWQiOiJmMzdlYmYzNy04NDAwLTQyZmYtYjc0MS04NGQ2YjYwOWZkNWMiLCJpc3MiOiJodHRwczovL2xvZ2ljaWRlbnRpdHlwcm9kLmIyY2xvZ2luLmNvbS9hYTBkNmYzYi04ZmIyLTQ3N2QtYWI4OC1iYjJlNjY0NjIwZTgvdjIuMC8iLCJleHAiOjE3Mjg0NzY3MTgsIm5iZiI6MTcyODQ3MzExOCwic3ViIjoiYjU1YjIzNzMtYjY4ZS00MDFhLWI0ZWQtY2YzZjFiMjdmYzkyIiwiaWRwIjoiTG9naWNJZGVudGl0eSIsImF1dGhlbnRpY2F0aW9uU291cmNlIjoiQ2xpZW50IENyZWRlbnRpYWxzIiwibm9uY2UiOiJkZWZhdWx0Tm9uY2UiLCJzY3AiOiJ1c2VyX2ltcGVyc29uYXRpb24iLCJhenAiOiJiNTViMjM3My1iNjhlLTQwMWEtYjRlZC1jZjNmMWIyN2ZjOTIiLCJ2ZXIiOiIxLjAiLCJpYXQiOjE3Mjg0NzMxMTh9.KU-hSJ7PoERHBTcI7IKUv4UH2s7-bV-i6f2TRswW0m8sAxLW_8vaLTf8GDE9dZSfyIke57v_Q7RPkVHqkBX7VKaJQazeyMHA3gPYI1N4SIdYbGhNxmriLaUUdw5585CAHYdvEQ9-Cm3Hf0czTp8WIxhSxIKCY31V85ZjdgW9a4z88ZJqVpcsSUCQ4gDi7c4tn035PMh12_s73nF2d2kPDjlKCjHAV4ZQetmFSp-4OXB-oieDOF9SzQ37Nk8CkS4ieDfUEm-lATbGEzN6V6Grlc9V-PrR7N3aXFBUOU3Ifbqivf0av0MbjsOAStH9j98e3p-zkFZdhPod2bJtnoKNGA";
+#endregion
 //var serviceCollection = new ServiceCollection();
 
 //serviceCollection.AddSemanticKernelLibrary();
@@ -15,6 +20,15 @@ using SemanticKernelLibrary;
 //var serviceProvider = serviceCollection.BuildServiceProvider();
 var config = new ConfigurationBuilder()
     .AddJsonFile("applicationsettings.json").Build();
+
+
+var httpclient = new HttpClient();
+httpclient.DefaultRequestHeaders.Authorization =
+new
+AuthenticationHeaderValue(
+"Bearer"
+, token);
+httpclient.DefaultRequestHeaders.TryAddWithoutValidation("Ocp-Apim-Subscription-Key", "6c15fd04d4504f4e89f10403de2f4299");
 
 OpenAIPromptExecutionSettings openAIPromptExecutionSettings = new()
 {
@@ -30,9 +44,13 @@ var kernelBuilder = Kernel.CreateBuilder().AddAzureOpenAIChatCompletion(
 kernelBuilder.Plugins.AddFromType<TimeInformationPlugin>();
 var kernel = kernelBuilder.Build();
 #pragma warning disable SKEXP0040 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
-await kernel.ImportPluginFromOpenApiAsync(
+var plugin = await kernel.ImportPluginFromOpenApiAsync(
     "studica",
-    new Uri(config["studica:url"])
+    new Uri(config["studica:url"]),
+    executionParameters: new OpenApiFunctionExecutionParameters()
+    {
+        HttpClient = httpclient
+    }
     );
 #pragma warning restore SKEXP0040 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
 
@@ -41,9 +59,10 @@ await kernel.ImportPluginFromOpenApiAsync(
 
 var chatCompletionService = kernel.GetRequiredService<IChatCompletionService>();
 ChatHistory history = [];
-
+history.AddSystemMessage($"When you use functions on the studica plugin, you will use this bearer token: {token}");
 while (true)
 {
+    Console.Write("User > ");
     var input = Console.ReadLine() ?? "";
     history.AddUserMessage(input);
 
@@ -54,5 +73,5 @@ while (true)
 
     history.Add(answer);
 
-    Console.WriteLine(answer);
+    Console.WriteLine($"GEORGE > {answer}");
 }
