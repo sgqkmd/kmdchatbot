dac1cef689d39f320a412f0bebb13c73f20e5862
Flemming Madsen <fm@kmd.dk>
2024-10-11 09:22:17 +0200

    Latest tweaks

--- AIConsole/Program.cs
+++ AIConsole/Program.cs
@@ -7,6 +7,8 @@ using Microsoft.SemanticKernel.ChatCompletion;
 using Microsoft.SemanticKernel.Connectors.OpenAI;
 using Microsoft.SemanticKernel.Plugins.OpenApi;
 using SemanticKernelLibrary;
+using System;
+using System.Linq.Expressions;
 using System.Net.Http.Headers;
 
 
@@ -34,16 +36,19 @@ var kernelBuilder = Kernel.CreateBuilder().AddAzureOpenAIChatCompletion(
                 );
 
 kernelBuilder.Plugins.AddFromType<TimeInformationPlugin>();
+kernelBuilder.Plugins.AddFromType<StudicaUiPlugin>();
+
 var kernel = kernelBuilder.Build();
+
 #pragma warning disable SKEXP0040 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
-//await kernel.ImportPluginFromOpenApiAsync(
-//    "studica",
-//    new Uri(config["studica:url"]),
-//    executionParameters: new OpenApiFunctionExecutionParameters()
-//    {
-//        HttpClient = httpclient
-//    }
-//    );
+await kernel.ImportPluginFromOpenApiAsync(
+    "students",
+    new Uri(config["studica-students:url"]),
+    executionParameters: new OpenApiFunctionExecutionParameters()
+    {
+        HttpClient = httpclient
+    }
+    );
 await kernel.ImportPluginFromOpenApiAsync(
     "absence",
     new Uri(config["studica-programmes:url"]),
@@ -54,11 +59,14 @@ await kernel.ImportPluginFromOpenApiAsync(
     );
 #pragma warning restore SKEXP0040 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
 
-
-
-
 var chatCompletionService = kernel.GetRequiredService<IChatCompletionService>();
 ChatHistory history = [];
+history.AddSystemMessage("Always assume school code should be A02332");
+history.AddSystemMessage("Always include a specific studentid when making requests for absence");
+history.AddSystemMessage("Set paramter OnlyAbsenceReports to true when making requests for absence");
+history.AddSystemMessage("Set StartDateFrom to 2024-01-01 and StartDateTo to 2024-12-31 when making requests for educational programmes");
+history.AddSystemMessage("Set pagesize to 1000 when this property is present in a request");
+history.AddSystemMessage("Ignore absence records with the status Not Registered when processing absence registration records");
 
 while (true)
 {
@@ -67,12 +75,26 @@ while (true)
     input = input == "" ? "Give me the top 5 students based on absence for school code A02332" : input;
     history.AddUserMessage(input);
 
-    var answer = await chatCompletionService.GetChatMessageContentAsync(
-        history,
-        openAIPromptExecutionSettings,
-        kernel);
+    ChatMessageContent answer = null;
+    try
+    {
+        answer = await chatCompletionService.GetChatMessageContentAsync(
+                history,
+                openAIPromptExecutionSettings,
+                kernel);
+    }
+    catch (HttpOperationException e) when (e.StatusCode == System.Net.HttpStatusCode.TooManyRequests)
+    {
+        Console.WriteLine("Too many request... retrying in 60 seconds");
+        await Task.Delay(60000);
+        answer = await chatCompletionService.GetChatMessageContentAsync(
+            history,
+            openAIPromptExecutionSettings,
+            kernel);
+    }
 
     history.Add(answer);
 
     Console.WriteLine($"GEORGE > {answer}");
+    Console.WriteLine();
 }
---
+++ AIConsole/StudicaUiPlugin.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.SemanticKernel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AIConsole
+{
+    internal class StudicaUiPlugin
+    {
+        [KernelFunction]
+        [Description("Show the absence page of a student in the Studica UI.")]
+        public void ShowStudentAbsence(string studentId)
+        {
+            var target = $"https://adhoc-preview.a.studica.dk/students/student/{studentId}/absence?absenceTab=courses";
+            
+            //url = url.Replace("&", "^&");
+            Process.Start(new ProcessStartInfo("cmd", $"/c start {target}") { CreateNoWindow = true });
+            
+            //Console.WriteLine("Open: " + target);
+            //Process.Start(target);
+        }
+    }
+}
